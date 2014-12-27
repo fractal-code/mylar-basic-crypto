3334682f61c40fadd17455408314bf8b1d4a9a86
Thomas Steinhauer <thomassteinhauer@gmail.com>
2014-12-28 00:30:04 +0100

    Added dependency for Underscore

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function (api) {
 
-    api.use('ejson');
+    api.use(['ejson', 'underscore']);
 
     api.addFiles(['sjcl.js', 'crypto.js'], ['client', 'server']);
 
