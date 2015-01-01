2a5def065bd166278ea3071c0f5d600e55e36b1f
Rami Jarrar <rami@grout.io>
2015-01-01 20:58:35 +0400

    Bump version to 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Basic crypto',
-    version: '0.1.0',
+    version: '0.1.1',
     name: 'mylar:basic-crypto',
     git: 'https://github.com/gliesesoftware/mylar-basic-crypto.git'
 });
